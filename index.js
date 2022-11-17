ab2c96ed5ea9efdd7d1fc3926b90ea0c379ddffc
vaish0207 <vaishu0299@gmail.com>
2022-11-17 06:05:54 -0500

    c

--- index.js
+++ index.js
@@ -1,166 +1,84 @@
 console.log("Starting Function");
-//import { Context, Callback } from 'aws-lambda';
-const sgMail = require("@sendgrid/mail");
-sgMail.setApiKey(
-  "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"
-);
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey("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");
 console.log("hi before");
-exports.handler = async function (event, context, callback) {
-  console.log("inside handler");
+    exports.handler = async function (event, context, callback) {
+    console.log("inside handler");
 
-  let message = event.Records[0].Sns.Message;
-  console.log("Message received from SNS:", message);
-  console.log("message from sns");
-  let data = JSON.parse(message);
-  console.log(data);
-  let fromAddress = data.fromAddress;
-  let toAddress = data.toAddress;
-  let userName = data.userName;
-  let link = data.link;
-  // let params = {
-  //   Destination:
-  //   {
-  //     ToAddresses: [toAddress],
-  //   },
-  //   Message:
-  //   {
-  //     Body:
-  //     {
-  //       Html:
-  //       {
-  //         Data:
-  //           ` <!DOCTYPE html>
-  //             <html>
-  //               <head>
-  //                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-  //                 <title>Account Verification</title>
-  //                 <style>
-  //                   body {
-  //                     background-color: #FFFFFF;
-  //                     padding: 10px;
-  //                     margin: 10px;
-  //                   }
-  //                 </style>
-  //               </head>
-  //               <body style="background-color: #FFFFFF; padding: 10px; margin: 10px;">
-  //               <article>
-  //                 <h1>
-  //                   Hi ${userName},
-  //                 </h1>
-  //                 <br>
-  //                 <h2>
-  //                   Welcome to ${data.domain},
-  //                 </h2>
-  //                 <p> Please user <a href=${link}>link</a> to verify your account.  </p>
-  //                 <br>
-  //                 <p> if you are unable to user the link, copy paste the below link in your browser</p>
-  //                 <p>${link}</p>
-  //               </article>
-  //               </body>
-  //             </html>`
-  //       },
-  //     },
-  //     Subject: { Data: `Account Verification - ${data.toAddress}` },
-  //   },
-  //   Source: fromAddress,
-  // };
-//   const msg = {
-//   	to: {
-//   		email: event?.to?.email,
-//   		name: event?.to?.name,
-//   	},
-//   	from: {
-//   		email: event?.from?.email,
-//   		name: event?.from?.name,
-//   	},
-//   	templateId: event.template_id,
-//   	dynamic_template_data: event.dynamic_template_data,
-//   };
-  const msg = {
-    to: toAddress,
-    from: "no-reply@demo.vaishnavisai.me", // Change to your verified sender
-    subject: "Sending with SendGrid is Fun",
-    text: "and easy to do anywhere, even with Node.js",
-    html: ` <!DOCTYPE html>
-              <html>
-                <head>
-                  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-                  <title>Account Verification</title>
-                  <style>
-                    body {
-                      background-color: #FFFFFF;
-                      padding: 10px;
-                      margin: 10px;
-                    }
-                  </style>
-                </head>
-                <body style="background-color: #FFFFFF; padding: 10px; margin: 10px;">
-                <article>
-                  <h1>
-                    Hi ${userName},
-                  </h1>
-                  <br>
-                  <h2>
-                    Welcome to demo.vaishnavisai.me,
-                  </h2>
-                  <p> Please user <a href=${link}>link</a> to verify your account.  </p>
-                  <br>
-                  <p> if you are unable to user the link, copy paste the below link in your browser</p>
-                  <p>${link}</p>
-                </article>
-                </body>
-              </html>`,
-  };
-console.log("after msg");
+    let message = event.Records[0].Sns.Message;
+    console.log('Message received from SNS:', message);
+    let data = JSON.parse(message);
+    console.log(data);
+    let fromAddress = data.fromAddress;
+    let toAddress = data.toAddress;
+    let userName = data.userName;
+    let link = data.link;
+    // let params = {
+    //   Destination: 
+    //   {
+    //     ToAddresses: [toAddress],
+    //   },
+    //   Message: 
+    //   {
+    //     Body: 
+    //     {
+    //       Html: 
+    //       {
+    //         Data:
+    //           ` <!DOCTYPE html>
+    //             <html>
+    //               <head>
+    //                 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    //                 <title>Account Verification</title>
+    //                 <style>
+    //                   body {
+    //                     background-color: #FFFFFF;
+    //                     padding: 10px;
+    //                     margin: 10px;
+    //                   }
+    //                 </style>
+    //               </head>
+    //               <body style="background-color: #FFFFFF; padding: 10px; margin: 10px;">
+    //               <article>
+    //                 <h1>
+    //                   Hi ${userName},
+    //                 </h1>
+    //                 <br>
+    //                 <h2>
+    //                   Welcome to ${data.domain},
+    //                 </h2>
+    //                 <p> Please user <a href=${link}>link</a> to verify your account.  </p>
+    //                 <br>
+    //                 <p> if you are unable to user the link, copy paste the below link in your browser</p>
+    //                 <p>${link}</p>
+    //               </article>
+    //               </body>
+    //             </html>`
+    //       },
+    //     },
+    //     Subject: { Data: `Account Verification - ${data.toAddress}` },
+    //   },
+    //   Source: fromAddress,
+    // };
 
-// const sgMail = require("@sendgrid/mail");
-// sgMail.setApiKey(
-//   "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"
-// );
 
-await sgMail.sendEmail(msg).promise();
-callback(null, "Success");
 
-//   sgMail
-//     .send(msg)
-//     .then((mailResponse) => {
-//       console.log("Email sent finally");
-//       callback(null, mailResponse);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//       callback("Error occurred while sending an email", error);
-//     });
+const msg = {
+  to: 'sachinbk0411@gmail.com', // Change to your recipient
+  from: 'vaishu0299@gmail.com', // Change to your verified sender
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+}
+console.log("after msg")
+sgMail
+  .send(msg)
+  .then((response) => {
+    console.log('Email sent');
+    callback(null, response);
+  })
+  .catch((error) => {
+    console.log(error)
+    callback(error)
+  })
 };
-// function getMessage() {
-//     const body = 'This is a test email using SendGrid from Node.js';
-//     return {
-//       to: "vaishu0299@gmail.com",
-//       from: 'no-reply@demo.vaishnavisai.me',
-//       subject: 'Test email with Node.js and SendGrid',
-//       text: body,
-//       html: `<strong>${body}</strong>`,
-//     };
-//   }
-  
-//   async function sendEmail() {
-//     try {
-//         console.log("inside try");
-//       await sgMail.send(getMessage());
-//       callback(null,"success");
-//       console.log('Test email sent successfully');
-//     } catch (error) {
-//       console.error('Error sending test email');
-//       console.error(error);
-//       if (error.response) {
-//         console.error(error.response.body)
-//       }
-//     }
-//   }
-//   console.log("async fun");
-//   (async () => {
-//     console.log('Sending test email');
-//     await sendEmail();
-//     console.log("after sendEmail");
-//   })();
-// }
